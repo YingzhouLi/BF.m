d4aad0a8d94d9c89fa4d6695bfd28a349b47ca02
lyzh588 <lyzh588@gmail.com>
2014-09-16 15:10:23 -0700

    Edit batch

--- test/run_hmbf.m
+++ test/run_hmbf.m
@@ -1,6 +1,4 @@
-close all;
-clear all;
-clc;
+function run_hmbf(N,func1_name,func2_name,FmR,mR,tol)
 
 addpath('../src/');
 data_path = '/home/ryanli/Documents/Research/HBF/data/';
@@ -12,12 +10,6 @@ end
 if(~exist(log_path, 'dir'))
     mkdir(log_path);
 end
-
-%% Set up parameters
-N = 128;
-tol=1e-6;
-mR = 25;         %max rank
-tukey_r = 0.1;
 
 k = -N/2:N/2-1;
 kbox = [-N/2,N/2;-N/2,N/2];
@@ -31,29 +23,10 @@ xbox = [0,1;0,1];
 x1s = x1s(:);  x2s = x2s(:);
 xx = [x1s x2s];
 
-func_name = 'fun0W';
-switch func_name
-    case 'funF'
-        fun = @funF;
-    case 'fun0'
-        fun = @fun0;
-    case 'fun0W'
-        fun = @(x,k)fun0W(x,k,N,tukey_r);
-        func_name = [func_name num2str(tukey_r)];
-    case 'fun1'
-        fun = @fun1;
-    case 'fun1W'
-        fun = @(x,k)fun1W(x,k,N,tukey_r);
-        func_name = [func_name num2str(tukey_r)];
-    case 'fun2'
-        fun = @fun2;
-    case 'fun2W'
-        fun = @(x,k)fun2W(x,k,N,tukey_r);
-        func_name = [func_name num2str(tukey_r)];
-end
+Factor1 = load([data_path 'Factors_' func1_name '_' num2str(N) '_' num2str(FmR) '.mat'],'Factors');
+%Factor2 = load([data_path 'Factors_' func2_name '_' num2str(N) '_' num2str(FmR) '.mat'],'Factors');
+Factor2=Factor1;
 
-%% Begin test
-if(1)
     if(1)
         f = randn(N,N) + sqrt(-1)*randn(N,N);
         binstr = sprintf('f_%d.bin', N);
@@ -70,14 +43,15 @@ if(1)
     f = reshape(f,N^2,1);
 
     tic;
-    y = zeros(N^2,1);
-    for i=1:N^2
-        y(i) = fun(xx(i,:),kk)*f;
-    end
+    y = apply_hbf(Factor1.Factors,f);
+    y = reshape(y,N,N,[]);
+    y = fft2(y)/N;
+    y = reshape(y,N^2,[]);
+    y = apply_hbf(Factor2.Factors,y);
     toc;
 
     tic;
-    Factors = hbf(N, fun, xx, xbox, kk, kbox, mR, tol, 1, 1);
+    Factors = hmbf(N, Factor1.Factors, Factor2.Factors, xx, xbox, kk, kbox, mR, tol, 1, 4);
     FactorT = toc;
 
     tic;
@@ -87,7 +61,8 @@ if(1)
 
     disp(['------------------------------------------']);
     disp(['N                 : ' num2str(N)]);
-    disp(['Function name     : ' func_name]);
+    disp(['Function1 name    : ' func1_name]);
+    disp(['Function2 name    : ' func2_name]);
     disp(['Max Rank          : ' num2str(mR)]);
     disp(['Tolerance         : ' num2str(tol)]);
     disp(['Relative Error_1  : ' num2str(norm(y-yy,1)/norm(y,1))]);
@@ -98,8 +73,8 @@ if(1)
     disp(['Applying Time     : ' num2str(ApplyT) ' s']);
     disp(['------------------------------------------']);
 
-    save([data_path 'Factors_' func_name '_' num2str(N) '_' num2str(mR) '.mat'],'Factors','-v7.3');
-    fid = fopen([log_path 'Factors_' func_name '_' num2str(N) '_' num2str(mR) '.log'],'w+');
+    save([data_path 'Factors_' func1_name '_' func2_name '_' num2str(N) '_' num2str(mR) '.mat'],'Factors','-v7.3');
+    fid = fopen([log_path 'Factors_' func1_name '_' func2_name '_' num2str(N) '_' num2str(mR) '.log'],'w+');
     fclose(fid);
-
+    
 end
